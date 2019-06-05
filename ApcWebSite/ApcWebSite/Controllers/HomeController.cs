f21aa640817b4e2802fd5631de013242c0726211
gracianow <gracianow@msn.com>
2019-06-05 17:43:14 -0300

    inclusão dos forms na apc

--- ApcWebSite/ApcWebSite/Controllers/HomeController.cs
+++ ApcWebSite/ApcWebSite/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ApcWebSite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +12,78 @@ namespace ApcWebSite.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.MostraSlide = false;
+            var model = new Contato();
+            model.Nome = "";
+
+            model.DataMensagem = DateTime.Now;
+            int tipo = 0;
+            string assunto = "";
+            model.TipoMensagem = tipo;
+            model.Assunto = assunto;
+
+
+            ViewBag.TipoMensagem = new SelectList(
+               new List<SelectListItem> {
+                  new SelectListItem { Value = "0" , Text = "Cotação"},
+                  new SelectListItem { Value = "1" , Text = "Atendimento"},
+                                             }, "Value", "Text", tipo);
+
+
+            ViewBag.Assunto = new SelectList(
+               new List<SelectListItem> {
+                                                new SelectListItem { Value = "0" , Text = "Selecione"},
+                                                new SelectListItem { Value = "1" , Text = "Site Moderno"},
+                                                new SelectListItem { Value = "2" , Text = "Anúncio Google meu Negócio"},
+                                                new SelectListItem { Value = "3" , Text = "Marketing Digital"},
+                                                new SelectListItem { Value = "4" , Text = "Internet WI-FI"},
+                                                new SelectListItem { Value = "5" , Text = "Mantutenção"},
+                                                new SelectListItem { Value = "6" , Text = "IRPF"},
+                                                new SelectListItem { Value = "7" , Text = "ERP"},
+                                                new SelectListItem { Value = "8" , Text = "Sugestão"},
+                                                new SelectListItem { Value = "9" , Text = "Pergunta"},
+                                                new SelectListItem { Value = "10" , Text = "Reclamação"},
+                                             }, "Value", "Text", assunto);
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(String nome, String email, String empresa, String celular, String foneFixo, String assunto, String tipo, String mensagem)
+        {
+            try
+            {
+                //tipo = ViewBag.TipoMensagem;
+                MailMessage correo = new MailMessage();
+                correo.From = new MailAddress("cotacao@allprotectioncorretora.com.br");
+                correo.To.Add("cotacao@allprotectioncorretora.com.br");
+                correo.Subject = assunto + " - " + nome;
+                correo.Body = "Nome: " + nome + "<br />" + "E-mail: " + email + "<br />" +
+                    "Empresa: " + empresa + "<br />" + "Celular: " + celular + "<br />" + "Fone: " + foneFixo + "<br />" + "Tipo de Mensagem: " + tipo + "<br />" +
+                    "Assunto: " + assunto + "<br />" + "Comentário: " + mensagem;
+                correo.IsBodyHtml = true;
+                correo.Priority = MailPriority.Normal;
+
+                //Configuração SMTP
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "mail.allprotectioncorretora.com.br";
+                smtp.Port = 587;
+                smtp.EnableSsl = false;
+                smtp.UseDefaultCredentials = true;
+                string conta = "cotacao@allprotectioncorretora.com.br";
+                string senha = "allpc2019";
+                smtp.Credentials = new System.Net.NetworkCredential(conta, senha);
+
+                smtp.Send(correo);
+                ViewBag.Mensagem = "Enviado com sucesso!";
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message + " - " + ex.StackTrace;
+            }
+
             return View();
         }
 
--- SiteDblv/SiteDblv/Controllers/ContatoController.cs
+++ SiteDblv/SiteDblv/Controllers/ContatoController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using SiteDblv.Models;
 using System.Net;
+using System.Web.Helpers;
 
 namespace SiteDblv.Controllers
 {
@@ -48,9 +49,36 @@ namespace SiteDblv.Controllers
                                                 new SelectListItem { Value = "9" , Text = "Pergunta"},
                                                 new SelectListItem { Value = "10" , Text = "Reclamação"},
                                              }, "Value", "Text", assunto);
+
+            var customerName = Request["customerName"];
+            var customerEmail = Request["customerEmail"];
+            var customerRequest = Request["customerRequest"];
+            var errorMessage = "";
+            var debuggingFlag = false;
+            try
+            {
+                // Initialize WebMail helper
+                WebMail.SmtpServer = "your-SMTP-host";
+                WebMail.SmtpPort = 25;
+                WebMail.UserName = "your-user-name-here";
+                WebMail.Password = "your-account-password";
+                WebMail.From = "your-email-address-here";
+
+                // Send email
+                WebMail.Send(to: customerEmail,
+                    subject: "Help request from - " + customerName,
+                    body: customerRequest
+                );
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+
             return View(model);
 
         }
+
 
         [HttpPost]
         public ActionResult Contact(Contato c)
@@ -65,13 +93,23 @@ namespace SiteDblv.Controllers
                     SmtpClient smtp = new SmtpClient();
                     MailAddress from = new MailAddress(c.Email.ToString());
                     StringBuilder sb = new StringBuilder();
-                    msg.IsBodyHtml = false;
+                    msg.IsBodyHtml = true;
                     SmtpClient client = new SmtpClient();
+
+                    //Instância classe email
+                    MailMessage mail = new MailMessage();
+                    msg.To.Add("contato@dobleve.com.br");
+                    msg.From = new MailAddress(c.Email);
+                    msg.Subject = c.Assunto;
+                    string Body = c.Mensagem;
+                    msg.Body = Body;
+                    mail.IsBodyHtml = true;
+
                     client.Credentials = new System.Net.NetworkCredential("contato@dobleve.com.br", "dblv2019#");
                     client.EnableSsl = true;
                     smtp.Host = "mail.dobleve.com.br";
                     smtp.UseDefaultCredentials = false;
-                    smtp.Port = 587;
+                    smtp.Port = 465;
 
                     msg.From = from;
                     msg.Subject = "Assunto: " + c.Assunto;
@@ -102,55 +140,54 @@ namespace SiteDblv.Controllers
         }
         /*
         [HttpPost]
-        public ActionResult FaleComigo(FormCollection collection)
+        public ViewResult Index(SiteDblv.Models.Contato _objModelMail)
         {
-            var fromNome = collection["name"].Trim();
-            var fromEmail = collection["email"].Trim();
-            var subject = " Mensagem do Site";
-            var body = collection["body"].Trim();
-
-            var to1 = "emailDestino1@hotmail.com";
-            var to2 = "emailDestino2@live.com";
-
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("<h2>Contato do Site XXXX</h2>");
-            sb.AppendLine("<h3>Nome: <b>" + fromNome + "</b> - E-mail: <b>" + fromEmail + "</b></h3>");
-            sb.AppendLine("<p>Mensagem:</p><br>");
-            sb.AppendLine(body);
-            try
+            if (ModelState.IsValid)
             {
-                using (MailMessage msg = new MailMessage())
-                {
-                    msg.From = new MailAddress("emailOrigem@gmail.com");
-                    msg.To.Add(to1);
-                    msg.Bcc.Add(to2);
-                    msg.Subject = "[Contato] " + subject;
-                    msg.Body = sb.ToString();
-                    msg.Priority = MailPriority.High;
-                    msg.IsBodyHtml = true;
+                MailMessage mail = new MailMessage();
+               //StringBuilder sb = new StringBuilder();
 
-                    using (SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587))
-                    {
-                        smtpClient.Credentials = new NetworkCredential("emailOrigem@gmail.com", "senhaEailOrigem");
-                        smtpClient.EnableSsl = true;
-                        smtpClient.Send(msg);
-                    }
-                }
+                //Instância classe email
 
-                return RedirectToAction("EmailEnviado");
+                mail.To.Add(_objModelMail.To);
+                mail.From = new MailAddress(_objModelMail.From);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                /*
+                sb.Append("Nome: " + c.Nome);
+                sb.Append(Environment.NewLine);
+                sb.Append("E-mail: " + c.Email);
+                sb.Append(Environment.NewLine);
+                sb.Append("Fone: " + c.Fone);
+                sb.Append(Environment.NewLine);
+                sb.Append("Data: " + c.DataMensagem);
+                sb.Append(Environment.NewLine);
+                sb.Append("Tipo: " + c.TipoMensagem);
+                sb.Append(Environment.NewLine);
+                sb.Append("Mensagem: " + c.Mensagem);
+                mail.Body = sb.ToString();
+                
+                smtp.Send(mail);
+                mail.Dispose();
+               
+
+                //Instância smtp do servidor, neste caso o gmail.
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "mail.dobleve.com.br";
+                smtp.Port = 465;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential
+                ("contato@dobleve.com.br", "dblv2019#");// Login e senha do e-mail.
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Index", _objModelMail);
             }
-            catch (Exception ex)
+            else
             {
-                ViewData.ModelState.AddModelError("_FORM", ex.ToString());
+                return View();
             }
-
-            return View();
-        }
-
-        public ActionResult EmailEnviado()
-        {
-            return View();
-        }
-        */
+        }*/
     }
 }
