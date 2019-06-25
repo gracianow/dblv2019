f1682d7a7daa018386def01a189c254304e148ae
gracianow <gracianow@msn.com>
2019-06-25 08:20:28 -0300

    inclusao perfume

---
+++ SiteDblv/SiteDblv/Controllers/PerfumeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteDblv.Controllers
+{
+    public class PerfumeController : Controller
+    {
+        // GET: Perfume
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
