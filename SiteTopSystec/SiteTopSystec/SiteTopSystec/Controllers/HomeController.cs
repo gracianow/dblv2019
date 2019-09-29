3a49be4481373b84b873a8a2b04cef33dc47ae29
gracianow <gracianow@msn.com>
2019-09-29 17:28:15 -0300

    atualizaçao topsystec pages

--- SiteTopSystec/SiteTopSystec/SiteTopSystec/Controllers/HomeController.cs
+++ SiteTopSystec/SiteTopSystec/SiteTopSystec/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace SiteTopSystec.Controllers
 {
+    [Tls]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -16,6 +17,23 @@ namespace SiteTopSystec.Controllers
         public ActionResult Default()
         {
             return View();
+        }
+
+        public class TlsAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                var request = filterContext.HttpContext.Request;
+                if (request.IsSecureConnection)
+                {
+                    filterContext.HttpContext.Response.AddHeader("Strict-Transport-Security", "max-age=15552000");
+                }
+                else if (!request.IsLocal && request.Headers["Upgrade-Insecure-Requests"] == "1")
+                {
+                    var url = new Uri("https://" + request.Url.GetComponents(UriComponents.Host | UriComponents.PathAndQuery, UriFormat.Unescaped), UriKind.Absolute);
+                    filterContext.Result = new RedirectResult(url.AbsoluteUri);
+                }
+            }
         }
 
         public ActionResult About()
